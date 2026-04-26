bfcea4f9d182540db55222820488f887df6cd1cd
4 0 4 \ 2.0 /🇲🇲 <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-04-27 02:43:09 +0630

    Update utils.js

--- functions/utils.js
+++ functions/utils.js
@@ -5,6 +5,7 @@
 
 import { PARSE_MODE } from './config.js';
 
+//Send Message Function
 export async function sendMessage(chatId, text, reply_markup, disable_web_page_preview, token, parse_mode = PARSE_MODE) {
     const url = `https://api.telegram.org/bot${token}/sendMessage`;
     const body = {
@@ -12,6 +13,29 @@ export async function sendMessage(chatId, text, reply_markup, disable_web_page_p
         text: text,
         parse_mode: parse_mode,
         disable_web_page_preview: disable_web_page_preview,
+    };
+
+    if (reply_markup) {
+        body.reply_markup = reply_markup;
+    }
+
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+}
+
+// Edit Message Function
+export async function editMessageText(chatId, messageId, text, reply_markup, token, parse_mode = PARSE_MODE) {
+    const url = `https://api.telegram.org/bot${token}/editMessageText`;
+    const body = {
+        chat_id: chatId,
+        message_id: messageId,
+        text: text,
+        parse_mode: parse_mode,
     };
 
     if (reply_markup) {
