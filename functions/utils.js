a0b0386f2ee234656d4a812f346e12241dc61aee
4 0 4 \ 2.0 /🇲🇲 <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-04-27 02:27:22 +0630

    Update utils.js

--- functions/utils.js
+++ functions/utils.js
@@ -5,15 +5,6 @@
 
 import { PARSE_MODE } from './config.js';
 
-/**
- * @param {number} chatId
- * @param {string} text
- * @param {Object | null} reply_markup
- * @param {boolean} disable_web_page_preview
- * @param {string} token
- * @param {string} parse_mode
- * @returns {Promise<Response>}
- */
 export async function sendMessage(chatId, text, reply_markup, disable_web_page_preview, token, parse_mode = PARSE_MODE) {
     const url = `https://api.telegram.org/bot${token}/sendMessage`;
     const body = {
